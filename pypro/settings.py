031fd277e651cc0da932bf8409090ad61cf4766c
Felipe Antonio <felipe.antonio.online@proton.me>
2022-07-07 16:22:08 -0300

    Fix
    Remove SECRET_KEY variable duplicated.

--- pypro/settings.py
+++ pypro/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
-from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,6 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
 if os.environ.get("SECRET_KEY", "") != "":
     # HTTP Strict Transport Security - careful now
     SECURE_HSTS_SECONDS = os.environ.get("SECURE_HSTS_SECONDS", 3600)
@@ -39,8 +39,6 @@ if os.environ.get("SECRET_KEY", "") != "":
 
     # Only run in debug mode if local
     DEBUG = False
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
