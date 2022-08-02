9a5b611f50be03458fb7772407ac64099d611009
Felipe Antonio <felipe.antonio.online@proton.me>
2022-08-02 11:46:23 -0300

    Update aperitivos view

--- aperitivos/views.py
+++ aperitivos/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, "aperitivos/video.html")
+    video = {"titulo": "Video Aperitivo: 7lions", "vimeo_id": 735480456}
+    return render(request, "aperitivos/video.html", context={"video": video})
