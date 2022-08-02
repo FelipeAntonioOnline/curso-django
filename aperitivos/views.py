3dc75e88217fa4daff9f91d0b45d17c79a69418a
Felipe Antonio <felipe.antonio.online@proton.me>
2022-08-02 13:39:20 -0300

    Update view to render videos based on slugs.

--- aperitivos/views.py
+++ aperitivos/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {"titulo": "Video Aperitivo: 7lions", "vimeo_id": 735480456}
+    videos = {
+        'motivacao': {"titulo": "Video Aperitivo: 7lions", "vimeo_id": 735480456},
+        'instalacao-windows': {"titulo": "Instalação Windows", "vimeo_id": 2511497668},
+    }
+    video = videos[slug]
     return render(request, "aperitivos/video.html", context={"video": video})
