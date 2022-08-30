8b08bef7a1e21e13b44069aca187bc2d30d8aa57
Felipe Antonio <felipe.antonio.online@proton.me>
2022-08-30 14:33:23 -0300

    Modify views to return 404 when page doesn't exist
    Update dependencies.

--- tests/aperitivos/test_video.py
+++ tests/aperitivos/test_video.py
@@ -2,19 +2,37 @@ import pytest
 from django.urls import reverse
 from django_assertions import assert_contains
 
+from aperitivos.models import Video
+
 
 @pytest.fixture
-def resp(client):
-    return client.get(reverse("aperitivos:video", args=("motivacao",)))
+def video(db):
+    v = Video(slug="motivacao", titulo="Video Aperitivo: 7lions", vimeo_id="735480456")
+    v.save()
+    return v
+
+
+@pytest.fixture
+def resp(client, video):
+    return client.get(reverse("aperitivos:video", args=(video.slug,)))
+
+
+@pytest.fixture
+def resp_video_nao_encontrado(client, video):
+    return client.get(reverse("aperitivos:video", args=(f"{video.slug}video_nao_existente",)))
 
 
 def test_status_code(resp):
     assert resp.status_code == 200
 
 
-def test_titulo_video(resp):
-    assert_contains(resp, "<title>Video Aperitivo: 7lions</title>", html=True)
+def test_titulo_video(resp, video):
+    assert_contains(resp, video.titulo, html=True)
 
 
-def test_iframe_video(resp):
-    assert_contains(resp, 'src="https://player.vimeo.com/video/735480456')
+def test_iframe_video(resp, video):
+    assert_contains(resp, f'src="https://player.vimeo.com/video/{video.vimeo_id}')
+
+
+def test_status_code_404(resp_video_nao_encontrado):
+    assert resp_video_nao_encontrado.status_code == 404
